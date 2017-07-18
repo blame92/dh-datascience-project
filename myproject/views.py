f36e45520450c48b710c97e508ff6b7ef6a4f163
Santi <siraola_ao@hotmail.com>
2017-07-18 21:40:03 +0000

    Recommendations working, result html with href working.

--- myproject/views.py
+++ myproject/views.py
@@ -3,22 +3,73 @@ from myproject import app
 from flask import render_template
 from flask import request
 import csv
+import json
+import unicodedata
 
-def looking_for_cluster(movie):
-    try:
-        ifile = open("./myproject/static/movie_clusterID.csv", "rb")
+def remove_accents(input_str):
+    nfkd_form = unicodedata.normalize('NFKD', input_str)
+    only_ascii = nfkd_form.encode('ASCII', 'ignore')
+    return only_ascii
+    
+def create_list_of_movies():
+    ifile = open("./myproject/static/df_lda_topics.csv", "rb")
+    reader = csv.reader(ifile)
+    movie_list = []
+    for row in reader:
+        movie_list.append(row[2])
+    ifile.close()
+    data = {}
+    data['movies'] = movie_list
+    with open('movie_list.json', 'w') as outfile:
+        json.dump(data, outfile)
+    
+def get_movie_link(movie_id,movie_name):
+    link = "http://www.cinemargentino.com/films/"
+    link_id = link + movie_id + "-"
+    m = movie_name.replace(" ", "-")
+    return link_id + m
+    
+def create_movie_json(movie_rec, original):
+    data = {}
+    data['list'] = movie_rec
+    data['original'] = original
+    with open('movie.json', 'w') as outfile:
+        json.dump(data, outfile)
+
+def looking_for_topic(movie):
+        ifile = open("./myproject/static/df_lda_topics.csv", "rb")
         reader = csv.reader(ifile)
-        test_list = ['hola']
+        recommended_list = []
+        topic_number = -1;
         for row in reader:
-            if movie in row[3]:
-                print movie
-                test_list.append(row[6])
+            try:
+                if movie == remove_accents(row[2].decode('utf-8')):
+                    
+                    topic_number = row[4]
+                    break;
+            except Exception as e:
+                print e
+        
+        for row in reader:
+            recommended_movie = {}
+            if topic_number == row[4]:
+                recommended_movie['name'] = row[2]
+                recommended_movie['id'] = row[0]
+                recommended_movie['topic'] = row[4]
+                recommended_movie['link'] = get_movie_link(row[0],row[2])
+                recommended_list.append(recommended_movie)
                 
         ifile.close()
-        return test_list
-    except:
-        print test_list
- 
+        create_movie_json(recommended_list,movie)
+        print len(recommended_list)
+        return recommended_list
+    
+
+def read_recommendations():
+    with open('movie.json', 'r') as movie_recom:
+        movies = json.load(movie_recom)
+    print movies
+    return movies
 
 ############################################# app.route ###########################################
 @app.route('/')
@@ -30,7 +81,7 @@ def index():
 @app.route('/loading', methods = ["POST"])
 def value():
     movie_input = request.form['movie']
-    cluster = looking_for_cluster(movie_input)
+    cluster = looking_for_topic(movie_input)
     return render_template('loading.html', movie = movie_input)
     
 
@@ -46,7 +97,12 @@ def topics():
 
 @app.route('/results', methods = ["GET","POST"])
 def results():
-    return render_template('results.html')
+    recommended_m = read_recommendations()
+    return render_template('results.html', recommendations1 = unicodedata.normalize('NFKD', recommended_m['list'][0]['name']).encode('ascii','ignore'),
+    recommendations2 = unicodedata.normalize('NFKD', recommended_m['list'][1]['name']).encode('ascii','ignore'), 
+    recommendations3 = unicodedata.normalize('NFKD', recommended_m['list'][2]['name']).encode('ascii','ignore'), original = recommended_m['original'],
+    cinema_link1 = recommended_m['list'][0]['link'], cinema_link2 = recommended_m['list'][1]['link'], cinema_link3 = recommended_m['list'][2]['link'])
+
 
 
 
